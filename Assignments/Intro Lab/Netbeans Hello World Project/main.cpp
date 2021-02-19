8c578321aa7d6f9ff3e11d47952be77769f3f3ff
as2890838 <asagaoinit1@student.rccd.edu>
2021-02-19 15:38:33 -0800

    making changes to intro lab hw

---
+++ Assignments/Intro Lab/Netbeans Hello World Project/main.cpp
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Arlene Sagaoinit
+ * Created on February 19, 2021, 10:50 AM
+ * Purpose:  First Program "Hello World"
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv){
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    cout<<"Hello World";
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
+
---
+++ Assignments/Intro Lab/Netbeans Hello World Project/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=netbeans_hello_world_project
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/netbeans_hello_world_project
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=netbeanshelloworldproject.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/netbeanshelloworldproject.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=netbeans_hello_world_project
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/netbeans_hello_world_project
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=netbeanshelloworldproject.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/netbeanshelloworldproject.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
